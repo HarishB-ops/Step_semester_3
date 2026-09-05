3975685a5871eb5294c5cbcb713469cf293856a8
Harish B <harishbalamurugan254@gmail.com>
2026-09-05 10:36:51 +0530

    Update FindMinimumInRotatedSortedArray.java

--- src/main/java/session4/assignment_problems/FindMinimumInRotatedSortedArray.java
+++ src/main/java/session4/assignment_problems/FindMinimumInRotatedSortedArray.java
@@ -1,20 +1,41 @@
-package session4.assignment_problems;
+import java.util.Scanner;
 
-/**
- * A5: Find Minimum in Rotated Sorted Array
- * Finds the minimum element in a rotated sorted array using modified binary search.
- */
-public class FindMinimumInRotatedSortedArray {
+public class FindMinimum {
 
     static int findMin(int[] nums) {
-        // TODO: binary search - at each step compare mid element to the rightmost element
-        // to decide which half the minimum must be hiding in
-        return 0;
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
     }
 
     public static void main(String[] args) {
-        System.out.println(findMin(new int[]{3, 4, 5, 1, 2}));
-        System.out.println(findMin(new int[]{4, 5, 6, 7, 0, 1, 2}));
-        System.out.println(findMin(new int[]{11, 13, 15, 17}));
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter rotated sorted array:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println("Minimum: " + findMin(nums));
     }
 }

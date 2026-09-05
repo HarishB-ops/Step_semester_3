969f646e1c423b225f1eac60b430dccee6514748
Harish B <harishbalamurugan254@gmail.com>
2026-09-05 11:36:39 +0530

    Update DuplicatePlayerPickChecker.java

--- src/main/java/session5/assignment_problems/DuplicatePlayerPickChecker.java
+++ src/main/java/session5/assignment_problems/DuplicatePlayerPickChecker.java
@@ -1,20 +1,31 @@
-package session5.class_problems;
-
-/**
- * Problem 2: Duplicate Player Pick Checker
- * Finds the first repeated player name in a lineup using nested loops.
- */
 public class DuplicatePlayerPickChecker {
 
     static String findDuplicatePick(String[] playerNames) {
-        // TODO: for each name, compare only against names that come after it (avoid checking pairs twice)
-        // return "Duplicate Found: <name>" for the first duplicate, scanning in order
-        // return "No Duplicates Found" if none exist
-        return "";
+
+        for (int i = 0; i < playerNames.length; i++) {
+
+            for (int j = i + 1; j < playerNames.length; j++) {
+
+                if (playerNames[i].equals(playerNames[j])) {
+                    return "Duplicate Found: " + playerNames[i];
+                }
+            }
+        }
+
+        return "No Duplicates Found";
     }
 
     public static void main(String[] args) {
-        System.out.println(findDuplicatePick(new String[]{"Kohli", "Bumrah", "Kohli", "Rohit"}));
-        System.out.println(findDuplicatePick(new String[]{"Kohli", "Bumrah", "Rohit"}));
+
+        String[] playerNames = {
+            "Kohli",
+            "Bumrah",
+            "Kohli",
+            "Rohit"
+        };
+
+        System.out.println(
+            findDuplicatePick(playerNames)
+        );
     }
 }

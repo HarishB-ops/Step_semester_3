21dce0a484b0f98eed4030eb939f0fee96ecb368
Harish B <harishbalamurugan254@gmail.com>
2026-09-05 11:37:11 +0530

    Update TopPerformerTracker.java

--- src/main/java/session5/assignment_problems/TopPerformerTracker.java
+++ src/main/java/session5/assignment_problems/TopPerformerTracker.java
@@ -1,18 +1,36 @@
-package session5.class_problems;
-
-/**
- * Problem 3: Top Performer Tracker
- * Finds min, max, and spread in a single pass without sorting.
- */
 public class TopPerformerTracker {
 
     static String findMinMaxSpread(int[] scores) {
-        // TODO: single pass - track running min and max together
-        // return "Min: X | Max: Y | Spread: Z" where Z = max - min
-        return "";
+
+        int min = scores[0];
+        int max = scores[0];
+
+        for (int i = 1; i < scores.length; i++) {
+
+            if (scores[i] < min) {
+                min = scores[i];
+            }
+
+            if (scores[i] > max) {
+                max = scores[i];
+            }
+        }
+
+        int spread = max - min;
+
+        return "Min: " + min
+             + " | Max: " + max
+             + " | Spread: " + spread;
     }
 
     public static void main(String[] args) {
-        System.out.println(findMinMaxSpread(new int[]{45, 82, 79, 90, 33, 90, 61}));
+
+        int[] scores = {
+            45, 82, 79, 90, 33, 90, 61
+        };
+
+        System.out.println(
+            findMinMaxSpread(scores)
+        );
     }
 }

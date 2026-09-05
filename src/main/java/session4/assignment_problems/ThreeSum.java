24c52fff1a26d662a13e4b76c3d4b4aab697106d
Harish B <harishbalamurugan254@gmail.com>
2026-09-05 10:34:36 +0530

    Update ThreeSum.java

--- src/main/java/session4/assignment_problems/ThreeSum.java
+++ src/main/java/session4/assignment_problems/ThreeSum.java
@@ -1,22 +1,83 @@
-package session4.assignment_problems;
+import java.util.*;
 
-import java.util.Arrays;
-
-/**
- * A3: 3Sum
- * Finds all unique triplets that sum to zero, using sort + two pointers.
- */
 public class ThreeSum {
 
     static int[][] threeSum(int[] nums) {
-        // TODO: sort the array first
-        // for each index i, use two pointers moving inward across the rest of the array
-        // to find pairs summing to -nums[i]; skip duplicate values at every level
-        return new int[0][0];
+
+        Arrays.sort(nums);
+
+        ArrayList<int[]> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    list.add(new int[]{
+                        nums[i], nums[left], nums[right]
+                    });
+
+                    while (left < right &&
+                           nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    while (left < right &&
+                           nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        int[][] result = new int[list.size()][];
+
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.deepToString(threeSum(new int[]{-1, 0, 1, 2, -1, -4})));
-        System.out.println(Arrays.deepToString(threeSum(new int[]{0, 0, 0})));
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int[][] result = threeSum(nums);
+
+        System.out.println("Triplets:");
+
+        for (int[] triplet : result) {
+            System.out.println(Arrays.toString(triplet));
+        }
     }
 }

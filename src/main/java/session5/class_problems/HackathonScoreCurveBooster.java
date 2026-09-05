cacfdad9ad7e469e69d24b55c5cc0326da715488
Harish B <harishbalamurugan254@gmail.com>
2026-09-05 11:32:12 +0530

    Update HackathonScoreCurveBooster.java

--- src/main/java/session5/class_problems/HackathonScoreCurveBooster.java
+++ src/main/java/session5/class_problems/HackathonScoreCurveBooster.java
@@ -1,20 +1,19 @@
-package session5.assignment_problems;
-
 import java.util.Arrays;
 
-/**
- * Assignment 1: Hackathon Score Curve Booster
- * Boosts every score in place by a flat bonus, then prints via Arrays.toString.
- */
 public class HackathonScoreCurveBooster {
 
     static void curveScores(int[] scores, int bonus) {
-        // TODO: modify scores in place - add bonus to every element, no return value
+        for (int i = 0; i < scores.length; i++) {
+            scores[i] = scores[i] + bonus;
+        }
     }
 
     public static void main(String[] args) {
+
         int[] scores = {70, 85, 60};
+
         curveScores(scores, 10);
+
         System.out.println(Arrays.toString(scores));
     }
 }
